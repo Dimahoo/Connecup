9db28fd06836e06cf226708e956af19d3c3b2bad
dimahoo <mlimouni82@gmail.com>
2020-07-12 16:02:28 -0400

    Add alert in contact send messages

--- dist/js/email.js
+++ dist/js/email.js
@@ -13,40 +13,41 @@ $(document).ready(function () {
         var statusElm = $('.status')
         statusElm.empty()
 
-        if(email.length > 5 && email.includes('@') && email.includes('.')) {
-            statusElm.append('<div>Email is valid</div>')
-        } else {
+        if (email.length < 5 && !email.includes('@') && !email.includes('.')) {
             event.preventDefault()
-            statusElm.append('<div>Email is not valid</div>')
+            document.getElementById('msg').style.height = "610px"
+            statusElm.append('<div id="alert" data-aos="fade-left">Email non valide</div>')
+        } else {
+
+            if (firstName.length < 2) {
+                event.preventDefault()
+                document.getElementById('msg').style.height = "610px"
+                statusElm.append('<div id="alert" data-aos="fade-left">Prénom non valide</div>')
+            } else {
+
+                if (lastName.length < 2) {
+                    event.preventDefault()
+                    document.getElementById('msg').style.height = "610px"
+                    statusElm.append('<div id="alert" data-aos="fade-left">Nom nom valide</div>')
+                } else {
+
+                    if (phone.length < 2) {
+                        event.preventDefault()
+                        document.getElementById('msg').style.height = "610px"
+                        statusElm.append('<div id="alert" data-aos="fade-left">Numéro de téléphone non valide</div>')
+                    } else {
+
+                        if (message.length < 20) {
+                            event.preventDefault()
+                            document.getElementById('msg').style.height = "610px"
+                            statusElm.append('<div id="alert" data-aos="fade-left">Message doit comporter plus de 20 caractères</div>')
+                        }
+                    }
+                }
+            }
         }
 
-        if(firstName.length > 2 ) {
-            statusElm.append('<div>First Name is valid</div>')
-        } else {
-            event.preventDefault()
-            statusElm.append('<div>First Name is not valid</div>')
-        }
-
-        if(lastName.length > 2 ) {
-            statusElm.append('<div>Last Name is valid</div>')
-        } else {
-            event.preventDefault()
-            statusElm.append('<div>Last Name is not valid</div>')
-        }
-
-        if(phone.length > 2 ) {
-            statusElm.append('<div>Phone is valid</div>')
-        } else {
-            event.preventDefault()
-            statusElm.append('<div>Phone is not valid</div>')
-        }
-
-        if(message.length > 20 ) {
-            statusElm.append('<div>Message is valid</div>')
-        } else {
-            event.preventDefault()
-            statusElm.append('<div>Message is not valid</div>')
-        }
+        document.getElementById('alert').style.color = 'red'
 
     })
 })
